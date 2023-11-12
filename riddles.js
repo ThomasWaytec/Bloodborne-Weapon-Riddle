164a65b15863e71f3977fe678d0b258822fb37c4
ThomasWaytec <thomas.waytec@gmail.com>
2023-11-12 17:43:25 +0100

    Create test riddle objects

---
+++ riddles.js
@@ -0,0 +1,33 @@
+const RIDDLES = [
+    {
+        "riddle": `I am shar, and serrated.
+                   I am a speedrunners best friend,
+                   Who am I?`,
+
+        "answers": [
+            "ReichterPallasch",
+            "Whirglig Saw",
+            "Holy Moonlight Sword",
+            "Saw Cleaver"
+        ],
+
+        "correct_answer": "Saw Cleaver"
+    },
+
+    {
+        "riddle": `I am the Ludwigs Holy blade.`,
+
+        "answers": [
+            "Ludwig's Holy Blade",
+            "Amygdalan Arm"
+        ],
+
+        "correct_answer": "Ludwig's Holy Blade"
+
+
+    }
+
+
+
+
+]
