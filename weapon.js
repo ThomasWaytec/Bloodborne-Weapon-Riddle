308e29e708a2b0cdd681c5c4c3b9b9c4c580bc7a
ThomasWaytec <thomas.waytec@gmail.com>
2023-11-12 18:16:35 +0100

    add 2 weapon instances

---
+++ weapon.js
@@ -0,0 +1,13 @@
+class Weapon {
+    constructor(name, imagePath) {
+      this.name = name;
+      this.imagePath = imagePath;
+    }
+}
+
+const HOLY_MOONLIGHT_SWORD = new Weapon("Holy Moonlight Sword", 
+                                        "assets/images/holy_moonlight_sword.png");
+
+const SAW_CLEAVER = new Weapon("Saw Cleaver",
+                               "assets/images/saw_cleaver.png");
+
